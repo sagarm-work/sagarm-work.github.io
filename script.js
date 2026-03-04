84fa9e897a77f60c9b3a198914b33390bde32e6c
sagarm-work <sagarmevada92@gmail.com>
2026-03-05 02:01:06 +0530

    upload new premium portfolio website files

---
+++ script.js
@@ -0,0 +1,15 @@
+ScrollReveal().reveal('.hero',{
+duration:1500,
+origin:'bottom',
+distance:'80px'
+})
+
+ScrollReveal().reveal('.app-card',{
+interval:200,
+distance:'60px',
+origin:'bottom'
+})
+
+ScrollReveal().reveal('.skill',{
+interval:100
+})
